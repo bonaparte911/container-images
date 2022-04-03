07948cb8a1f9a8018502336eea755c4bdaa59f48
bonaparte <thebonaparte@arcor.de>
2022-04-03 21:16:58 +0200

    fixed Windows file endings

--- apps/osemu/entrypoint.sh
+++ apps/osemu/entrypoint.sh
@@ -5,6 +5,11 @@ source "/shim/umask.sh"
 source "/shim/vpn.sh"
 source "/shim/greeting.sh"
 
+# fix Windows line-endings
+sed $'s/\r$//' /shim/umask.sh > /shim/umask.sh
+sed $'s/\r$//' /shim/vpn.sh > /shim/vpn.sh
+sed $'s/\r$//' /shim/greeting.sh > /shim/greeting.sh
+
 FOO="${OSEMU_USER:=user}"  
 FOO="${OSEMU_PASSWORD:=password}"
 
