dfec9bc524ec6e5148bca0a44cc17bd73d4003c9
bonaparte <thebonaparte@arcor.de>
2022-06-13 21:04:32 +0200

    moved julia extensions to start-up scripts, because otherwise installed for root

--- apps/openvscode-julia-cuda/shim/config.sh
+++ apps/openvscode-julia-cuda/shim/config.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p /config
+
+echo "**** install extensions ****" && \
+    install-extension julialang.language-julia && \
+    install-extension singularitti.vscode-julia-formatter && \
+    install-extension bungcip.better-toml && \
